202fc0f637fc4493ce4b30efa20004533f521c12
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2020-12-13 18:32:15 +0100

    Script Added

---
+++ Password Generator/main.py
@@ -0,0 +1,27 @@
+import random
+uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+lowercase_letters = uppercase_letters.lower()
+digits = '1234567890'
+symbols = '@#$%=.,:;'
+
+upper,lower,nums,syms = True,True,True,True
+
+all = ''
+
+if upper:
+    all += uppercase_letters
+if lower:
+    all += lowercase_letters
+if nums:
+    all += digits
+if syms:
+    all += symbols
+
+print('How long you want your password?')
+length = int(input())
+print('How many password do you want to generate?')
+passwords = int(input())
+for x in range(passwords):
+    password = "".join(random.sample(all, length))
+    print(password)
+print('Done')
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Python-Scripts
+# Python Scripts
+
 This is a repository with all my scripts in Python
+
+### Scripts
+
+- Password Generator 13/12/2020
