ec23421b65005ce495a0999cabf41dbed792ba30
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2020-12-16 20:12:50 +0100

    Script Added

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ This is a repository with all my scripts in Python
 
 - Password Generator **13/12/2020**
 - Wifi Password Extractor **13/12/2020**
-- Random Identity Generator (Not Finished) **15/12/2020**
+- Random Identity Generator **16/12/2020**
--- Random Identity Generator/main.py
+++ Random Identity Generator/main.py
@@ -9,6 +9,12 @@ possible_ages = random.randint(20,60)
 possible_jobs = ['Student', 'Taxi Driver','Bus Driver','Teacher','Programmer','Chef','Caisher']
 mail_domains = ['@gmail.com','@outlook.com','@yahoo.com']
 possible_countries = ['EE.UU','Spain','Canada','Russia','France','India','England']
+possible_phone_prefixes = ['+1','+34','+7','+33','+91','+44']
+phone_numbers = str(random.randint(1,99)) + str(random.randint(1,99)) + str(random.randint(1,99))
+possible_weight = random.randint(50,80)
+possible_heigth = random.randint(150,190) / 100
+possible_relationship = ['Single','Married','Not interested']
+relationship_interests = ['Boys','Girls']
 class Person:
     name = random.choice(possible_male_names)
     age = possible_ages
@@ -16,9 +22,38 @@ class Person:
     job = random.choice(possible_jobs)
     email = name.lower() + str(birth_year) + random.choice(mail_domains)
     country = random.choice(possible_countries)
-print(Person.name)
-print(Person.age)
-print(Person.birth_year)
-print(Person.job)
-print(Person.email)
-print(Person.country)
+    phone = ''
+    weight = str(possible_weight) + 'kg'
+    heigth = str(possible_heigth) + 'm'
+    relationship = random.choice(possible_relationship)
+    interested = random.choice(relationship_interests)
+
+if Person.country == possible_countries[0] or Person.country == possible_countries[2]:
+    Person.phone = possible_phone_prefixes[0] + phone_numbers
+else:
+    if Person.country == possible_countries[1]:
+        Person.phone = possible_phone_prefixes[1] + phone_numbers
+    else:
+        if Person.country == possible_countries[3]:
+            Person.phone = possible_phone_prefixes[3] + phone_numbers
+        else:
+            if Person.country == possible_countries[4]:
+                Person.phone = possible_phone_prefixes[4] + phone_numbers
+            else:
+                if Person.country == possible_countries[5]:
+                    Person.phone = possible_phone_prefixes[5] + phone_numbers
+
+if Person.relationship == possible_relationship[2]:
+    relationship_interests = 'Unkwon'
+
+print('Name: ' + Person.name)
+print('Age: ' + str(Person.age))
+print('Birth Year: ' + str(Person.birth_year))
+print('Job: ' + Person.job)
+print('Email: ' + Person.email)
+print('Country: ' + Person.country)
+print('Phone Number: ' + Person.phone)
+print('Weigth: ' + Person.weight)
+print('Height: ' + Person.heigth)
+print('Relationship: ' + Person.relationship)
+print('Interested in: ' + Person.interested)
