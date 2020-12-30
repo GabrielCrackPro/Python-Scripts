45c45be605b5523ad9d1eae541ad45ad37403326
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2020-12-30 16:03:31 +0100

    Script Updated

--- Random Identity Generator/main.py
+++ Random Identity Generator/main.py
@@ -3,18 +3,54 @@ import datetime
 
 actual_year = datetime.datetime.now().year
 
-possible_male_names = ['Fredrick','John','Coleman','Johan','Raymond']
-possible_female_names = ['Gaelle', 'Leilany','Ann','Alison','Pam']
-possible_ages = random.randint(20,60)
-possible_jobs = ['Student', 'Taxi Driver','Bus Driver','Teacher','Programmer','Chef','Caisher']
+#FILES
+
+#MALE NAMES LIST
+male_names = open('male_names.txt','r')
+male_names_list = []
+male_names_lines = male_names.readlines()
+for line in male_names_lines:
+    male_names_list.append(line.strip('\n').split('\n'))
+
+#FEMALE NAMES LIST
+female_names = open('female_names.txt', 'r')
+female_names_list = []
+female_names_lines = female_names.readlines()
+for line in female_names_lines:
+    female_names_list.append(line.strip('\n').split('\n'))
+
+#JOB LIST
+jobs = open('job_list.txt','r')
+job_list = []
+jobs_lines = jobs.readlines()
+for line in jobs_lines:
+    job_list.append(line.strip('\n').split('\n'))
+
+#COUNTRIES LIST
+countries = open('countries.txt', 'r')
+countries_list = []
+for line in countries:
+    countries_list.append(line.strip('\n').split('\n'))
+
+#CARS LIST
+cars = open('cars.txt','r')
+car_list = []
+for line in cars:
+    car_list.append(line.strip('\n').split('\n'))
+
+possible_male_names = random.choice(male_names_list)
+possible_female_names = random.choice(female_names_list)
+possible_ages = random.randint(10,99)
+possible_jobs = random.choice(job_list)
 mail_domains = ['@gmail.com','@outlook.com','@yahoo.com']
-possible_countries = ['EE.UU','Spain','Canada','Russia','France','India','England']
+possible_countries = random.choice(countries_list)
 possible_phone_prefixes = ['+1','+34','+7','+33','+91','+44']
 phone_numbers = str(random.randint(1,99)) + str(random.randint(1,99)) + str(random.randint(1,99))
 possible_weight = random.randint(50,80)
 possible_heigth = random.randint(150,190) / 100
 possible_relationship = ['Single','Married','Not interested']
 relationship_interests = ['Boys','Girls']
+possible_car_makes = random.choice(car_list)
 class Person:
     name = random.choice(possible_male_names or possible_female_names)
     age = possible_ages
@@ -27,6 +63,7 @@ class Person:
     heigth = str(possible_heigth) + 'm'
     relationship = random.choice(possible_relationship)
     interested = random.choice(relationship_interests)
+    car = random.choice(possible_car_makes)
 
 if Person.country == possible_countries[0] or Person.country == possible_countries[2]:
     Person.phone = possible_phone_prefixes[0] + phone_numbers
@@ -42,9 +79,11 @@ else:
             else:
                 if Person.country == possible_countries[5]:
                     Person.phone = possible_phone_prefixes[5] + phone_numbers
+                else:
+                    Person.phone = phone_numbers
 
 if Person.relationship == possible_relationship[2]:
-    relationship_interests = 'Unkwon'
+   Person.interested = 'Unkwon'
 
 print('Name: ' + Person.name)
 print('Age: ' + str(Person.age))
@@ -53,6 +92,7 @@ print('Job: ' + Person.job)
 print('Email: ' + Person.email)
 print('Country: ' + Person.country)
 print('Phone Number: ' + Person.phone)
+print('Car: ' + Person.car)
 print('Weigth: ' + Person.weight)
 print('Height: ' + Person.heigth)
 print('Relationship: ' + Person.relationship)
